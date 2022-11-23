5ab93461f9366afc82f0b9efb15cd239b83ea181
Olha Prymak <113449585+peckeby@users.noreply.github.com>
2022-11-23 17:36:27 +0200

    Make reference file

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -12,7 +12,7 @@
         .then(response => {
         if (!response.ok) {
           throw new Error(response.status);
-        }
+        };
         return response.json();
         })
     }
--- src/index.js
+++ src/index.js
@@ -3,23 +3,18 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import createCoutryItem from './create-country-item.hbs';
 import debounce from 'lodash.debounce';
 import { FetchCountriesApi } from './fetchCountries';
+import {refs} from './refs';
 
 const DEBOUNCE_DELAY = 300;
 
 const countriesApi = new FetchCountriesApi;
 
-
-const searchBox = document.querySelector('#search-box');
-const coutryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
-
-
 const onChangeForm = (event) => {
   const eventTargetValue = event.target.value;
   countriesApi.searchCountryName = eventTargetValue.trim();
   if (event.target.value === ""){
-    coutryList.innerHTML = "";
-    countryInfo.innerHTML = "";
+    refs.coutryList.innerHTML = "";
+    refs.countryInfo.innerHTML = "";
     return;
   }
   countriesApi
@@ -27,30 +22,30 @@ const onChangeForm = (event) => {
     .then(data => {
       if (data.length > 10){
         Notify.info('Too many matches found. Please enter a more specific name.')
-        coutryList.innerHTML = "";
-        countryInfo.innerHTML = "";
+        refs.coutryList.innerHTML = "";
+        refs.countryInfo.innerHTML = "";
         return;
       }
 
       if (data.length === 1) {
         const languagesValues = Object.values(data[0].languages);
-        coutryList.innerHTML = createCoutryItem(data);
-        countryInfo.innerHTML =
+        refs.coutryList.innerHTML = createCoutryItem(data);
+        refs.countryInfo.innerHTML =
         `<p class="info-partial"><span class="category-of-info">Official name:</span> ${data[0].name.official}</p>
         <p class="info-partial"><span class="category-of-info">Capital:</span> ${data[0].capital}</p>
         <p class="info-partial"><span class="category-of-info">Population:</span> ${data[0].population}</p>
         <p class="info-partial"><span class="category-of-info">Languages:</span> ${languagesValues}</p>`
         return
       }
-      coutryList.innerHTML = createCoutryItem(data)
-      countryInfo.innerHTML = "";
+      refs.coutryList.innerHTML = createCoutryItem(data)
+      refs.countryInfo.innerHTML = "";
     })
     .catch(err => {
       console.log(err);
-      coutryList.innerHTML = "";
-      countryInfo.innerHTML = "";
+      refs.coutryList.innerHTML = "";
+      refs.countryInfo.innerHTML = "";
       Notify.failure('Oops, there is no country with that name')});
 }
 
-searchBox.addEventListener('input', debounce((event) => {
+refs.searchBox.addEventListener('input', debounce((event) => {
   onChangeForm(event)}, `${DEBOUNCE_DELAY}`));
